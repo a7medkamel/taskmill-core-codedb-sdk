f524350a6dd06a946984ab9da4d8c6968a4a296a
Ahmed Kamel <ak@ahmedkamel.net>
2017-04-23 14:06:44 -0700

    add she endpoint and support bearer option

--- index.js
+++ index.js
@@ -11,17 +11,19 @@ var Promise         = require('bluebird')
 
 const url = config.has('codedb')? config.getUrl('codedb') : undefined;
 
-function blob(remote, filename, options) {
-  let body = {
-      remote          : remote
-    , filename        : filename
-    , branch          : options.branch
-    , token           : options.token
-    , populate        : options.populate
-  };
+function blob(remote, filename, options = {}) {
+  let { branch, populate, token, bearer } = options;
+
+  let body    = { remote, filename, token, branch, populate }
+    , headers = {}
+    ;
+
+  if (bearer) {
+    headers['Authorization'] = bearer;
+  }
 
   return Promise
-          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/blob'), { body : body, json : true }))
+          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/blob'), { body, json : true, headers }))
           .then((result) => {
             let ret = _.pick(result, 'content', 'manual', 'branch', 'path', 'repository');
 
@@ -35,18 +37,19 @@ function blob(remote, filename, options) {
           });
 }
 
-function hotreload(remote, filename, content, options) {
-  let body = {
-      remote          : remote
-    , filename        : filename
-    , content         : content
-    , branch          : options.branch
-    , token           : options.token
-    , populate        : options.populate
-  };
+function hotreload(remote, filename, content, options = {}) {
+  let { branch, populate, token, bearer } = options;
+
+  let body    = { remote, filename, content, token, branch, populate }
+    , headers = {}
+    ;
+
+  if (bearer) {
+    headers['Authorization'] = bearer;
+  }
 
   return Promise
-          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/blob/hotreload'), { body : body, json : true }))
+          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/blob/hotreload'), { body, json : true, headers }))
           .then((result) => {
             let ret = _.pick(result, 'content', 'manual', 'branch', 'path', 'repository');
 
@@ -60,54 +63,80 @@ function hotreload(remote, filename, content, options) {
           });
 }
 
-function ls(remote, options) {
-  let body = {
-      remote          : remote
-    , token           : options.token
-    , branch          : options.branch
-    , populate        : options.populate
-  };
+function ls(remote, options = {}) {
+  let { branch, populate, token, bearer } = options;
+
+  let body    = { remote, token, branch, populate }
+    , headers = {}
+    ;
+
+  if (bearer) {
+    headers['Authorization'] = bearer;
+  }
 
   return Promise
-          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/ls'), { body : body, json : true }))
+          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/ls'), { body, json : true, headers }))
           .catch(errors.StatusCodeError, (err) => {
             throw new Error('not found');
           });
 }
 
-function pull(remote, options) {
-  let body = {
-      remote          : remote
-    , token           : options.token
-    , branch          : options.branch
-    , populate        : options.populate
-  };
+function pull(remote, options = {}) {
+  let { branch, populate, token, bearer } = options;
+
+  let body    = { remote, token, branch, populate }
+    , headers = {}
+    ;
+
+  if (bearer) {
+    headers['Authorization'] = bearer;
+  }
 
   return Promise
-          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/pull'), { body : body, json : true }))
+          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/pull'), { body, json : true, headers }))
+          .catch(errors.StatusCodeError, (err) => {
+            throw new Error('not found');
+          });
+}
+
+function sha(remote, options = {}) {
+  let { branch, token, bearer } = options;
+
+  let body    = { remote, token, branch }
+    , headers = {}
+    ;
+
+  if (bearer) {
+    headers['Authorization'] = bearer;
+  }
+
+  return Promise
+          .resolve(rp.post(urljoin(url || _.get(options, 'url'), '/sha'), { body, json : true, headers }))
           .catch(errors.StatusCodeError, (err) => {
             throw new Error('not found');
           });
 }
 
 function archive(remote, options = {}) {
-  let body = {
-      remote          : remote
-    , token           : options.token
-    , branch          : options.branch
-    , make            : options.make
-  };
+  let { branch, make, token, bearer } = options;
 
-  let headers = {};
+  let body    = { remote, token, branch, make }
+    , headers = {}
+    ;
+
   if (options.ifnonematch) {
     headers['If-None-Match'] = options.ifnonematch    
+  }
+
+  if (bearer) {
+    headers['Authorization'] = bearer;
   }
 
   return request({
               url     : urljoin(url || _.get(options, 'url'), '/archive')
             , method  : 'POST' 
-            , body    : body
-            , headers : headers
+            , body
+            , headers
             , json    : true
           });
 }
@@ -117,5 +146,6 @@ module.exports = {
   , hotreload : hotreload
   , ls        : ls
   , pull      : pull
+  , sha       : sha
   , archive   : archive
 };
